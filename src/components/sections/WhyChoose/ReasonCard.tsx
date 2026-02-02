b72a7590f899e34e6ff7e879cf92a2e10385c0e5
fantee-hub <tobifaniran@gmail.com>
2026-02-02 18:36:55 +0100

    animation for how it works

--- src/components/sections/WhyChoose/ReasonCard.tsx
+++ src/components/sections/WhyChoose/ReasonCard.tsx
@@ -1,4 +1,14 @@
-import { motion } from "framer-motion";
+import { Variants, motion } from "framer-motion";
+
+const itemVariants: Variants = {
+  hidden: { opacity: 0, y: 30 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.6, ease: "easeOut" },
+  },
+};
+
 const ReasonCard = ({
   title,
   description,
@@ -8,7 +18,13 @@ const ReasonCard = ({
   description: string;
   icon: string;
 }) => (
-  <div className="bg-white p-6 flex flex-col items-start text-left">
+  <motion.div
+    variants={itemVariants}
+    className="bg-white p-6 flex flex-col items-start text-left border-r-0 border-b border-[#E6E6E6] 
+    last:border-b-0 last:border-r-0 
+    md:border-r md:nth-[2n]:border-r-0 md:nth-last-[-n+2]:border-b-0
+    lg:nth-[2n]:border-r lg:nth-[3n]:border-r-0 lg:nth-last-[-n+3]:border-b-0"
+  >
     <div className="w-16 h-16 bg-[#E4D6FE] rounded-3xl flex items-center justify-center mb-8 ">
       <motion.div className="rounded-[20px] bg-[#7632F9] w-14 h-14 shadow-lg shadow-purple-100 flex items-center justify-center">
         <motion.img
@@ -31,7 +47,7 @@ const ReasonCard = ({
       {title}
     </h3>
     <p className="text-[#5C5C5C] leading-6">{description}</p>
-  </div>
+  </motion.div>
 );
 
 export default ReasonCard;
--- src/components/sections/WhyChoose/index.tsx
+++ src/components/sections/WhyChoose/index.tsx
@@ -1,6 +1,18 @@
 "use client";
 
 import ReasonCard from "./ReasonCard";
+import { motion } from "framer-motion";
+import { ScrollReveal } from "../../ScrollReveal";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.15,
+    },
+  },
+};
 
 const reasons = [
   {
@@ -45,21 +57,29 @@ const WhyChoose = () => {
   return (
     <section className="w-full bg-white page border-b border-[#E6E6E6] mt-10 lg:mt-0 px-4 lg:px-0 border-t lg:border-t-0">
       <div className="border-x border-[#E6E6E6] mx-auto max-w-300">
-        <div className="flex flex-col items-center px-6 lg:px-10 pt-10 lg:pt-20 pb-8 lg:pb-10 text-center">
-          <span className="mb-2 rounded-full bg-[#FAFAFA] lg:px-6 lg:py-4 px-4 py-2 font-semibold text-[#7632F9]">
-            Why Choose Federated Engineers
-          </span>
-          <h2 className="lg:max-w-4xl lg:text-[48px] text-[28px] font-semibold lg:leading-14 leading-8 text-[#2F1464]">
-            A Collaborative Engineering <br className="hidden lg:block" /> Pool
-            You Can Trust
-          </h2>
-          <p className="lg:max-w-139.5 pt-2 text-[#5C5C5C]">
-            Our pool brings vetted engineers, shared standards, and structured
-            collaboration to deliver dependable data and AI support.
-          </p>
-        </div>
+        <ScrollReveal>
+          <div className="flex flex-col items-center px-6 lg:px-10 pt-10 lg:pt-20 pb-8 lg:pb-10 text-center">
+            <span className="mb-2 rounded-full bg-[#FAFAFA] lg:px-6 lg:py-4 px-4 py-2 font-semibold text-[#7632F9]">
+              Why Choose Federated Engineers
+            </span>
+            <h2 className="lg:max-w-4xl lg:text-[48px] text-[28px] font-semibold lg:leading-14 leading-8 text-[#2F1464]">
+              A Collaborative Engineering <br className="hidden lg:block" />{" "}
+              Pool You Can Trust
+            </h2>
+            <p className="lg:max-w-139.5 pt-2 text-[#5C5C5C]">
+              Our pool brings vetted engineers, shared standards, and structured
+              collaboration to deliver dependable data and AI support.
+            </p>
+          </div>
+        </ScrollReveal>
 
-        <div className="grid grid-cols-1 border-t border-[#E6E6E6] bg-[#E6E6E6] md:grid-cols-2 lg:grid-cols-3 gap-px">
+        <motion.div
+          variants={containerVariants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: false, amount: 0.2 }}
+          className="grid grid-cols-1 border-t border-[#E6E6E6] md:grid-cols-2 lg:grid-cols-3 "
+        >
           {reasons.map((reason, index) => (
             <ReasonCard
               key={index}
@@ -68,7 +88,7 @@ const WhyChoose = () => {
               icon={reason.icon}
             />
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
